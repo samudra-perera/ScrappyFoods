80f695cedce12330dc0ce7bc5c200b5a5c3ba107
Samudra Perera <samudrapup@gmail.com>
2023-06-24 23:09:48 -0400

    Fixed the firebase auth component, added further functionality to signup componen

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,26 +1,30 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useState, useEffect } from "react";
 import { auth } from "../firebase";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 export function useAuth() {
-    return useContext(AuthContext)
+  return useContext(AuthContext);
 }
 
-export const AuthProvider = ({children}) => {
-    const [currentUser, setCurrentUser] = useState()
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
 
-    const signUp = (email, password) => {
-        return auth.createUserWithEmailAndPasword(email, password)
-    }
+  const signup = (email, password) => {
+    return auth.createUserWithEmailAndPassword(email, password)
+  };
 
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      setCurrentUser(user);
+    });
 
-    const value = {
-        currentUser
-    }
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+    return unsubscribe
+  });
+
+  const value = {
+    currentUser,
+    signup
+  };
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
